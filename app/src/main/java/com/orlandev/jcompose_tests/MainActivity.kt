7d1ecf18f0b0fe3068b6fea70a5010d2ded9b2e8
ondev <dfashion.corp@gmail.com>
2022-02-06 21:40:49 -0600

    Extract component

--- app/src/main/java/com/orlandev/jcompose_tests/MainActivity.kt
+++ app/src/main/java/com/orlandev/jcompose_tests/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.orlandev.jcompose_tests.components.ModalBottomSheetSample
 import com.orlandev.jcompose_tests.ui.theme.Jcompose_testsTheme
 import kotlinx.coroutines.launch
 
@@ -34,41 +35,5 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-@OptIn(ExperimentalMaterialApi::class)
-fun ModalBottomSheetSample() {
-    val state = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
-    val scope = rememberCoroutineScope()
-    ModalBottomSheetLayout(
-        sheetState = state,
-        sheetContent = {
-            LazyColumn {
-                items(50) {
-                    ListItem(
-                        text = { Text("Item $it") },
-                        icon = {
-                            Icon(
-                                Icons.Default.Favorite,
-                                contentDescription = "Localized description"
-                            )
-                        }
-                    )
-                }
-            }
-        }
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Rest of the UI")
-            Spacer(Modifier.height(20.dp))
-            Button(onClick = { scope.launch { state.show() } }) {
-                Text("Click to show sheet")
-            }
-        }
-    }
-}
+
 
